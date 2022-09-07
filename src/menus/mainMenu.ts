70a4a1b64566da10f8a1937b886f4cfe2271afa8
umutaliev <mutaliyevutkirbek@gmail.com>
2022-09-07 14:16:25 +0500

    inline keyboard chnaged

--- src/menus/mainMenu.ts
+++ src/menus/mainMenu.ts
@@ -3,14 +3,18 @@ import { onCommandInfo } from "../handlers/commands/onCommandInfo";
 import { onCommandHelp } from "../handlers/commands/onCommandHelp";
 import { InlineKeyboard } from "grammy";
 export const mainMenu = new InlineKeyboard()
+  .url("Connect with Admin", "https://t.me/jakhongirabdukhamidov")
+  .row()
   .url(
-    "Menu",
+    "Our Menu",
     "https://drive.google.com/file/d/1K_SessS8Dx7nQP1DaDYiBSma1ZzG84ia/view"
-  ).row()
-  .text("Info", "info").row()
-  .text("Help", "help").row()
-  .text("About", "about").row()
-  .text("Help", "help");
+  )
+  .row()
+  .text("Our Working Hours", "help")
+  .row()
+  .url("Connect with Owner", "https://t.me/jakhongirabdukhamidov")
+  .row()
+  .url("Connect with Owner", "https://t.me/jakhongirabdukhamidov");
 
 
 
